8ee5819fd36d2e807be388f47356634f846aea45
mohammed kaif <mkaif555123@gmail.com>
2024-07-02 18:42:53 +0500

    ehfjgd

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // while loop
 
-let i = 1;
+// let i = 1;
 
-while (i <= 10) {
-    console.log(i);
-    i++;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+//events link
+
+function kaif(){
+    console.log("kaif");
 }
