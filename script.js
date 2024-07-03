55afb5f59157bdec0918c40d0c8a7d3d11f7c655
mohammed kaif <mkaif555123@gmail.com>
2024-07-03 16:53:19 +0500

    create element practice and learning complete

--- script.js
+++ script.js
@@ -43,9 +43,18 @@ const parent = document.querySelector(".parent");
 // console.log(parent.firstElementChild);
 // console.log(parent.lastElementChild);
 
-const days = document.querySelector(".day");
+// const days = document.querySelector(".day");
 
 // console.log(days.parentElement);
 // console.log(days.nextElementSibling);
 
-console.log("NODES: " , parent.childNodes);
+// console.log("NODES: " , parent.childNodes);
+
+const div = document.createElement("h1");
+div.className = "main"
+div.id = Math.round(Math.random() * 10 + 1)
+div.setAttribute("title", "kaif")
+// div.innerText = "practice DOM"
+const addText = document.createTextNode("parctice DOM");
+div.appendChild(addText)
+console.log(div);
