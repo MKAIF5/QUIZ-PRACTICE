039e0e82006943af2c2a33e629fe8b8d0aa8fdc2
mohammed kaif <mkaif555123@gmail.com>
2024-07-03 13:41:02 +0500

    dom manipulation pratcice'

--- script.js
+++ script.js
@@ -17,7 +17,14 @@
 
 const title = document.getElementById("title")
 
-title.style.backgroundColor = "green"
-title.style.height = "100px"
-title.style.padding = "10px"
-title.style.borderRadius = "10px"
+// title.style.backgroundColor = "green"
+// title.style.height = "100px"
+// title.style.padding = "10px"
+// title.style.borderRadius = "10px"
+
+ const a = title.innerHTML
+ const b = title.innerText
+ const c = title.textContent
+ console.log(a);
+console.log(b);
+console.log(c);
