28dc460a17ac11a98792c9d897903d8162cad20c
mohammed kaif <mkaif555123@gmail.com>
2024-07-04 11:12:50 +0500

    commenting out

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
 // while loop
 
-// let i = 1;
+let i = 1;
 
-// while (i <= 10) {
-//     console.log(i);
-//     i++;
-// }
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
 
-//events link
 
-// function kaif(){
-//     console.log("kaif");
-// }
+function kaif(){
+    console.log("kaif");
+}
 
 // DOM
 
 const title = document.getElementById("title")
 
-// title.style.backgroundColor = "green"
-// title.style.height = "100px"
-// title.style.padding = "10px"
-// title.style.borderRadius = "10px"
+title.style.backgroundColor = "green"
+title.style.height = "100px"
+title.style.padding = "10px"
+title.style.borderRadius = "10px"
 
-// const a = title.innerHTML
-// const b = title.innerText
-// const c = title.textContent
-// console.log(a);
-// console.log(b);
-// console.log(c);
+const a = title.innerHTML
+const b = title.innerText
+const c = title.textContent
+console.log(a);
+console.log(b);
+console.log(c);
 
 //Dom Create new element
 
 const parent = document.querySelector(".parent");
 
-// console.log(parent.children[0].innerHTML);
+console.log(parent.children[0].innerHTML);
 
-// for (i = 0; i < parent.children.length; i++) {
-//     console.log(parent.children[i].innerHTML);
-// }
+for (i = 0; i < parent.children.length; i++) {
+    console.log(parent.children[i].innerHTML);
+}
 
-// parent.children[1].style.color = "orange";
-// console.log(parent.firstElementChild);
-// console.log(parent.lastElementChild);
+parent.children[1].style.color = "orange";
+console.log(parent.firstElementChild);
+console.log(parent.lastElementChild);
 
-// const days = document.querySelector(".day");
+const days = document.querySelector(".day");
 
-// console.log(days.parentElement);
-// console.log(days.nextElementSibling);
+console.log(days.parentElement);
+console.log(days.nextElementSibling);
 
-// console.log("NODES: " , parent.childNodes);
+console.log("NODES: " , parent.childNodes);
 
-// const div = document.createElement("h1");
-// div.className = "main"
-// div.id = Math.round(Math.random() * 10 + 1)
-// div.setAttribute("title", "kaif")
-// // div.innerText = "practice DOM"
-// const addText = document.createTextNode("parctice DOM");
-// div.appendChild(addText)
-// console.log(div);
+const div = document.createElement("h1");
+div.className = "main"
+div.id = Math.round(Math.random() * 10 + 1)
+div.setAttribute("title", "kaif")
+// div.innerText = "practice DOM"
+const addText = document.createTextNode("parctice DOM");
+div.appendChild(addText)
+console.log(div);
 
-// function addLanguage(lang) {
-//     const li = document.createElement("li")
-//     li.innerHTML = lang
-//     document.querySelector('.language').appendChild(li)
-// }
+function addLanguage(lang) {
+    const li = document.createElement("li")
+    li.innerHTML = lang
+    document.querySelector('.language').appendChild(li)
+}
 
-// addLanguage("python")
-// addLanguage("typescript")
+addLanguage("python")
+addLanguage("typescript")
 
 //OBJECTS
 const mySym = Symbol("mykey1")
@@ -81,14 +80,14 @@ const myObjects = {
     siblings: { sister: "1", brother: "0" }
 }
 
-// console.log(myObjects.location);
-// console.log(myObjects[location]);
-// console.log(myObjects[mySym]);
+console.log(myObjects.location);
+console.log(myObjects[location]);
+console.log(myObjects[mySym]);
 
-// Object.freeze(myObjects)
-// myObjects.email = "kaif@google.com"
-// myObjects.email = "kaif@chatgpt.com"
-// console.log(myObjects);
+Object.freeze(myObjects)
+myObjects.email = "kaif@google.com"
+myObjects.email = "kaif@chatgpt.com"
+console.log(myObjects);
 
 const kaifApp = new Object();
 const kaifApps = {};
