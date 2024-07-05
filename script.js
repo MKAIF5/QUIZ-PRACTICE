db466c2037194dd224cd6b859e6200945ec3de21
mohammed kaif <mkaif555123@gmail.com>
2024-07-05 20:33:40 +0500

    local storage practice

--- script.js
+++ script.js
@@ -102,9 +102,9 @@
 const key = prompt("enetr key you want to set");
 const value = prompt("enter value you want to set");
 
-localStorage.setItem(key , value)
+localStorage.setItem(key, value)
 console.log(` the value is set ${key} is  ${localStorage.getItem(key)}`)
 
-if(key == "red" || key == "blue"){
+if (key == "red" || key == "blue") {
     localStorage.removeItem(key)
 }
