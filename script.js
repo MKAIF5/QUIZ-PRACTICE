147b9674287b78156e3d23ee0a208b59043439e1
mohammed kaif <mkaif555123@gmail.com>
2024-07-06 09:59:43 +0500

    blur prctice

--- script.js
+++ script.js
@@ -146,3 +146,19 @@
 //     console.log(website);
 // }
 // console.log(username);
+
+// function fillCity() {
+//      var cityName;
+//      var zipEntered = document.getElementById("zip").value;
+//      switch (zipEntered) {
+//      case "60608" :
+//      cityName = "Chicago";
+//      break;
+//      case "68114" :
+//      cityName = "Omaha";
+//     break;
+//      case "53212" :
+//     cityName = "Milwaukee";
+// }
+// document.getElementById("city").value = cityName;
+//      }
